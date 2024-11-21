90344b4354bfbb130ea7e8f0cd0ea3d765e09af1
jk <jeje9893@gmail.com>
2024-11-21 14:38:59 +0900

    프로젝트 업데이트

---
+++ svelte-project/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Router, Route, Link } from "svelte-routing";
+
+  // 페이지 컴포넌트 임포트
+  import Home from './pages/Home.svelte';
+  import About from './pages/About.svelte';
+  import Contact from './pages/Contact.svelte';
+</script>
+
+<Router>
+  <!-- 내비게이션 링크 -->
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+
+  <!-- 라우팅 설정 -->
+  <Route path="/" component={Home} />
+  <Route path="/about" component={About} />
+  <Route path="/contact" component={Contact} />
+</Router>
