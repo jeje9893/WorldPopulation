88d1a93efdbb19d9942c426bd2518da4eaf25469
jk <jeje9893@gmail.com>
2024-11-26 12:47:31 +0900

    헬로월드 버튼 추가

--- svelte-project/src/pages/Home.svelte
+++ svelte-project/src/pages/Home.svelte
@@ -1,2 +1,8 @@
 <h1>Home Page</h1>
 <p>Welcome to the Home page!</p>
+ 
+<script>
+    document.getElementById("myButton").addEventListener("click", function() {
+        alert("Hello, World!");
+    });
+</script>
