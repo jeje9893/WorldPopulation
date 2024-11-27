b3e50326f28fcc6f334ca7752bf4b401e3a6c9dc
jk <jeje9893@gmail.com>
2024-11-27 14:47:12 +0900

    곽우석 버튼 추가

--- svelte-project/src/pages/Home.svelte
+++ svelte-project/src/pages/Home.svelte
@@ -2,6 +2,11 @@
   import { onMount } from 'svelte';
   let data = [];
   let country = 'usa'; // 원하는 국가로 변경 가능
+  let showText = false;
+
+  function handleClick() {
+    showText = true;
+  }
 
   onMount(async () => {
     const res = await fetch(`http://localhost/WorldPopulation/phpPart/makingDB.php?country=${country}`);
@@ -11,6 +16,11 @@
 
 <h1>Home Page</h1>
 <p>Welcome to the Home page!</p>
+
+<button on:click={handleClick}>버튼</button>
+{#if showText}
+  <h1>곽우석</h1>
+{/if}
 
 {#each data as item}
   <p>{item.year}: {item.population}</p>
